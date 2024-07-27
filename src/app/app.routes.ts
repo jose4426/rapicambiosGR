afe35cad25c13393474cc3030f2a19d6bc2e09ba
Jose Gonzalez <gonzalezj231@gmail.com>
2024-07-27 06:24:12 -0400

    cambiosgr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CambiosComponent } from './cambios/cambios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component: CambiosComponent
+    },{
+        path:'',
+        component: CambiosComponent
+    }
+];
---
+++ src/app/cambios/cambios.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cambios',
+  standalone: true,
+  imports: [],
+  styleUrl: './cambios.component.css',
+  templateUrl: './cambios.component.html'
+
+})
+export class CambiosComponent {
+
+}
